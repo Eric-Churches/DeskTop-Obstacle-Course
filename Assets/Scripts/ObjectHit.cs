04432ff6d28183a6ec6c588f15fb9bec3edc634d
Eric Churches <59458107+Eric-Churches@users.noreply.github.com>
2022-07-03 13:15:38 +1200

    Revert "Added sound to the rocket"
    This reverts commit c445066d8479f983ad56bfefad7ce1b65fc61759.

--- Assets/Scripts/ObjectHit.cs
+++ Assets/Scripts/ObjectHit.cs
@@ -6,11 +6,7 @@ public class ObjectHit : MonoBehaviour
 {
    private void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            GetComponent<MeshRenderer>().material.color =Color.magenta;
-            gameObject.tag = "Hit";
-        }
-       
+        Debug.Log("You bumped into the wall");
+        GetComponent<MeshRenderer>().material.color =Color.magenta;
     }
 }
